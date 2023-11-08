d0600ea0916a0cf090456c72ba83d92841cca844
Ben Lynch <bdlynch7@gmail.com>
2023-11-07 18:20:47 -0600

    finished problem 3

--- 003_largest_prime_factor.js
+++ 003_largest_prime_factor.js
@@ -1,22 +1,26 @@
 // The prime factors of 13195 are 5, 7, 13, 29
 // What is the largest prime factor of the number 600851475143?
 
-let factors = [];
-let primeFactors = []
+let factors = []
+let answer = factors[factors.length-1]
 
-function getFactors(num) {
-  for (i = 2; i < num; i++) {
-    if (num % i == 0) {
-      factors.push(i);
+function getPrimeFactors(num) {
+  while (num % 2 == 0) 
+  {
+    num = num/2
+  }
+  for (let i=3; i<num; i++)
+  {
+    while (num % i == 0)
+    {
+      factors.push(i)
+      num = num / i
     }
   }
+  if (num > 2) {
+    factors.push(num)
+  }  
 }
 
-getFactors(13195);
-console.log(factors);
-
-function getPrimeFactors(array) {
-  for (num of array) {
-    
-  }
-}
+getPrimeFactors(600851475143)
+console.log(factors[factors.length-1])
